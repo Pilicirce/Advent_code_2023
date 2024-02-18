d2b0792d3f0595e503e92b835b1017c77848222e
Pilar Solis <pilar.sm02@gmail.com>
2024-02-18 19:12:17 +0100

    toquetear la estructura y el pom.xml

--- src/main/com/adventcode2023/app/AdventCodeApp.java
+++ src/main/com/adventcode2023/app/AdventCodeApp.java
@@ -1,14 +1,24 @@
 public class AdventCodeApp {
 
-    //this is the main class of the project. It indicates which Day and Part of the Adven Code must run
+    // this is the main class of the project. It indicates which Day and Part of the
+    // Adven Code must run
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Use: java AdventCodeApp <Day> <Part>");
-            return;
-        }
 
-        int day = Integer.parseInt(args[0]);
-        int part = Integer.parseInt(args[1]);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce what DAY of the Adven Code Calendar would you like to run (for example, 1): ");
+        int day = scanner.nextInt();
+
+        System.out.print("Introduce what PART of the exercise would you like to run (1 o 2): ");
+        int part = scanner.nextInt();
+
+        // if (args.length != 2) {
+        // System.out.println("Use: java AdventCodeApp <Day> <Part>");
+        // return;
+        // }
+
+        // int day = Integer.parseInt(args[0]);
+        // int part = Integer.parseInt(args[1]);
 
         switch (day) {
             case 1:
@@ -20,13 +30,27 @@ public class AdventCodeApp {
                     System.out.println("Invalid Part");
                 }
                 break;
-//            case 2:
-//                // Lógica para el día 2
-//                break;
+
+            // case 2:
+            // if (part == 1) {
+            // Day2Part1.solve();
+            // } else if (part == 2) {
+            // Day2Part2.solve();
+            // } else {
+            // System.out.println("Invalid Part");
+            // }
+            // break;
+
+            // case 3:
+            // // Lógica para el día 2
+            // break;
+
             // Otros casos para diferentes días
             default:
                 System.out.println("Invalid Day");
         }
+
+        scanner.close();
     }
-    
+
 }
--- src/main/com/adventcode2023/day1/Day1_Part2.java
+++ src/main/com/adventcode2023/day1/Day1_Part2.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Day1_Part2 {
 
-    public static void main(String[] args) {
+    public static void mainDay1Part2(String[] args) {
         String inputFilePath = "C:\\Users\\msolisma\\Desktop\\PILI\\CURSOS\\AdventCode\\ejercicioDia1.txt";
         String outputFilePath = "C:\\Users\\msolisma\\Desktop\\PILI\\CURSOS\\AdventCode\\ejercicioDia1b_modificado.txt";
 
@@ -23,47 +23,56 @@ public class Day1_Part2 {
 
             // 3) Declara variables
             String linea;
-            List<Integer> numerosDosDigitos = new ArrayList<>(); // Lista para almacenar los números de dos dígitos en un array
+            List<Integer> numerosDosDigitos = new ArrayList<>(); // Lista para almacenar los números de dos dígitos en
+                                                                 // un array
 
-            // 4) Itera sobre cada línea del archivo y reemplaza las palabras por números (en un método a parte)
+            // 4) Itera sobre cada línea del archivo y reemplaza las palabras por números
+            // (en un método a parte)
             while ((linea = reader.readLine()) != null) {
                 // 4.a) Reemplaza las palabras con números
-                linea = reemplazarPalabrasConNumeros(linea).trim(); // Elimina los espacios en blanco al inicio y al final de la línea
+                linea = reemplazarPalabrasConNumeros(linea).trim(); // Elimina los espacios en blanco al inicio y al
+                                                                    // final de la línea
 
                 // 4.c) Escribe la línea modificada en el archivo de salida
                 writer.write(linea);
                 writer.newLine(); // Añade una nueva línea después de cada línea modificada
 
-                
-                // 5) Encuentra el primer dígito (número) en cada línea: 
+                // 5) Encuentra el primer dígito (número) en cada línea:
                 if (!linea.isEmpty()) { // para evitar procesar líneas en blanco
 
-                   // 5.a) Primero, encuentra el índice del primer dígito que sea un número
+                    // 5.a) Primero, encuentra el índice del primer dígito que sea un número
                     int indicePrimerDigito = 0;
-                    while (indicePrimerDigito < linea.length() && !Character.isDigit(linea.charAt(indicePrimerDigito))) {
+                    while (indicePrimerDigito < linea.length()
+                            && !Character.isDigit(linea.charAt(indicePrimerDigito))) {
                         indicePrimerDigito++;
-                        // esto verifica si el carácter en la posición actual (indicePrimerDigito) de la cadena linea es un dígito o no. Character.isDigit() es un método que devuelve
-                        // true si el carácter proporcionado es un dígito, y false de lo contrario. Pero como he puesto un (!), es al reves. Entonces, el bucle se ejecuta mientras en
-                        // indice del caracter que analizo sea menor que la lingitud del string linea Y mientras que el caracter no sea un dígito.
+                        // esto verifica si el carácter en la posición actual (indicePrimerDigito) de la
+                        // cadena linea es un dígito o no. Character.isDigit() es un método que devuelve
+                        // true si el carácter proporcionado es un dígito, y false de lo contrario. Pero
+                        // como he puesto un (!), es al reves. Entonces, el bucle se ejecuta mientras en
+                        // indice del caracter que analizo sea menor que la lingitud del string linea Y
+                        // mientras que el caracter no sea un dígito.
                     }
 
-                    //5.b) Luego coge el valor de ese índice en la cadena y lo almacenamos
+                    // 5.b) Luego coge el valor de ese índice en la cadena y lo almacenamos
                     char primerDigito = linea.charAt(indicePrimerDigito);
-                    // este método coge un índice (indicePrimerDigito) y devuelve el carácter en esa posición dentro de la cadena linea. En términos sencillos, esto extrae el
-                    // carácter en la posición del primer dígito encontrado y lo guarda en la variable primerDigito.
+                    // este método coge un índice (indicePrimerDigito) y devuelve el carácter en esa
+                    // posición dentro de la cadena linea. En términos sencillos, esto extrae el
+                    // carácter en la posición del primer dígito encontrado y lo guarda en la
+                    // variable primerDigito.
 
                     // 6) Encuentra el último dígito (número) entre las letras
-                    //6.a) Primero, encuentra el índice del último dígito que sea un número
+                    // 6.a) Primero, encuentra el índice del último dígito que sea un número
                     int indiceUltimoDigito = linea.length() - 1;
                     while (indiceUltimoDigito >= 0 && !Character.isDigit(linea.charAt(indiceUltimoDigito))) {
                         indiceUltimoDigito--;
-                        // esto es lo mismo que con el primer dígito, pero empieza por atras (lenth-1) y se va restando índices hasta que llega a 0.
-                        // porque en realidad, estoy recorriendo el string otra vez, pero de atras para alante
+                        // esto es lo mismo que con el primer dígito, pero empieza por atras (lenth-1) y
+                        // se va restando índices hasta que llega a 0.
+                        // porque en realidad, estoy recorriendo el string otra vez, pero de atras para
+                        // alante
                     }
 
-                    //6.b) Luego coge el valor de ese índice en la cadena y lo almacenamos
+                    // 6.b) Luego coge el valor de ese índice en la cadena y lo almacenamos
                     char ultimoDigito = linea.charAt(indiceUltimoDigito);
-
 
                     // 7) Convierte los caracteres a dígitos y crea el número de dos dígitos
                     int numeroDosDigitosLinea = Integer.parseInt("" + primerDigito + ultimoDigito);
@@ -71,13 +80,12 @@ public class Day1_Part2 {
                     // 8) Almacena el número de dos dígitos en la lista
                     numerosDosDigitos.add(numeroDosDigitosLinea);
 
-
-                // Muestra el número de dos dígitos obtenido de la línea
-                System.out.println("Número de dos dígitos de la línea: " + numeroDosDigitosLinea);
+                    // Muestra el número de dos dígitos obtenido de la línea
+                    System.out.println("Número de dos dígitos de la línea: " + numeroDosDigitosLinea);
+                }
             }
-    }
 
-            //9)  Cierra los recursos
+            // 9) Cierra los recursos
             reader.close();
             writer.close();
 
@@ -87,19 +95,15 @@ public class Day1_Part2 {
                 suma += num;
             }
 
-             System.out.println("Proceso completado. El archivo modificado se encuentra en: " + outputFilePath);
+            System.out.println("Proceso completado. El archivo modificado se encuentra en: " + outputFilePath);
 
             // 11) Muestra la suma total de los números de dos dígitos
             System.out.println("Suma total: " + suma);
-
-            
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 
     // 4.b) fuera de la clase main: metodo para reemplazar palabras por numeros
     private static String reemplazarPalabrasConNumeros(String linea) {
@@ -115,26 +119,26 @@ public class Day1_Part2 {
     }
 
     // private static int encontrarPrimerDigito(String linea) {
-    //     for (char c : linea.toCharArray()) {
-    //         if (Character.isDigit(c)) {
-    //             return Character.getNumericValue(c);
-    //         }
-    //     }
-    //     return 0; // Retorna 0 si no se encuentra ningún dígito
+    // for (char c : linea.toCharArray()) {
+    // if (Character.isDigit(c)) {
+    // return Character.getNumericValue(c);
+    // }
+    // }
+    // return 0; // Retorna 0 si no se encuentra ningún dígito
     // }
 
     // private static int encontrarUltimoDigito(String linea) {
-    //     for (int i = linea.length() - 1; i >= 0; i--) {
-    //         char c = linea.charAt(i);
-    //         if (Character.isDigit(c)) {
-    //             return Character.getNumericValue(c);
-    //         }
-    //     }
-    //     return 0; // Retorna 0 si no se encuentra ningún dígito
+    // for (int i = linea.length() - 1; i >= 0; i--) {
+    // char c = linea.charAt(i);
+    // if (Character.isDigit(c)) {
+    // return Character.getNumericValue(c);
+    // }
+    // }
+    // return 0; // Retorna 0 si no se encuentra ningún dígito
     // }
 
-    // private static int obtenerNumeroDosDigitos(int primerDigito, int ultimoDigito) {
-    //     return primerDigito * 10 + ultimoDigito;
+    // private static int obtenerNumeroDosDigitos(int primerDigito, int
+    // ultimoDigito) {
+    // return primerDigito * 10 + ultimoDigito;
     // }
 }
-
